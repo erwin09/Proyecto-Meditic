158817fccfd0a7d7cbd7ecb5e94869079aa1a9c1
meditic o46 <johan.antolinez09@outlook.com>
2022-08-31 20:00:18 -0500

    primer commit

---
+++ src/main/java/mintic/edu/java/backendspring/BackendspringApplication.java
@@ -0,0 +1,13 @@
+package mintic.edu.java.backendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mintic/edu/java/backendspring/BackendspringApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.edu.java.backendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
